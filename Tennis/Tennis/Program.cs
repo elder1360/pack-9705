6cadb16c7920fba5faaef71aea1945542b3fc439
Elder1360 <mohamm60@outlook.com>
2018-09-30 00:28:51 +0330

    -a

--- Tennis/Tennis/Program.cs
+++ Tennis/Tennis/Program.cs
@@ -17,8 +17,8 @@ namespace Tennis
         {
             Console.CursorVisible = false;
             Console.Title = "TENNIS SCOREBOARD";
-            Console.WindowHeight = 25;
-            Console.WindowWidth = 130;
+            Console.WindowHeight = 30;
+            Console.WindowWidth = 135;
             Console.SetWindowPosition(0, 0);
             Console.SetCursorPosition(10, 30);
             Console.BackgroundColor = ConsoleColor.White;
@@ -73,15 +73,23 @@ namespace Tennis
             {
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.SetCursorPosition(Console.WindowWidth / 2 - 30, Console.WindowHeight / 2);                
-                Console.WriteLine($"{player[0]} Starts The Game!    Press LEFT Arrow key to start!");
+                Console.SetCursorPosition(Console.WindowWidth / 2 - 18, Console.WindowHeight / 2);                
+                Console.WriteLine($"{player[0]} Starts The Game!(Left ArrowKey)");
+                Console.SetCursorPosition(Console.WindowWidth / 2 - 18, Console.WindowHeight / 2+1);
+                Console.WriteLine("         Press Any Key To Start!");
+                Console.ReadKey();
+
             }
             else
             {
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Blue;
-                Console.SetCursorPosition(Console.WindowWidth / 2 - 30, Console.WindowHeight / 2);                
-                Console.WriteLine($"{player[1]} Starts The Game!    Press RIGHT Arrow key to start!");
+                Console.SetCursorPosition(Console.WindowWidth / 2 - 18, Console.WindowHeight / 2);                
+                Console.WriteLine($"{player[1]} Starts The Game!(Right ArrowKey)");
+                Console.SetCursorPosition(Console.WindowWidth / 2 - 18, Console.WindowHeight / 2+1);
+                Console.WriteLine("         Press Any Key To Start!");
+                Console.ReadKey();
+                
             }
             bool match = false;
             int i = 0;
@@ -99,6 +107,7 @@ namespace Tennis
                     byte pointP2 = 0;
                     while (true)
                     {
+                        ScoreBoard(pointP1, pointP2);
                         ConsoleKey pressedKey = Console.ReadKey().Key;
                         if (pressedKey==ConsoleKey.LeftArrow)
                         {
@@ -114,10 +123,16 @@ namespace Tennis
                         }
                         else
                         {
+                            Console.Clear();
                             Console.SetCursorPosition(Console.WindowWidth / 2 - 12, Console.WindowHeight / 2);
                             Console.ForegroundColor = ConsoleColor.Black;
                             Console.WriteLine("please enter left or right arrow key!");
-                            
+                            for (int j = 0; j < 2; j++)
+                            {
+                                Console.Beep(3000, 50);
+                                Thread.Sleep(50);
+                            }
+                            Thread.Sleep(1000);
                             continue;
                         }
                         if ((pointP1 > pointP2 + 1) && pointP1 > 3)
@@ -134,7 +149,6 @@ namespace Tennis
                     }
                     GameP1History[i] = gameP1;
                     GameP2History[i] = gameP2;                    
-                    ScoreBoard(pointP1, pointP2);
                     if (gameP1>gameP2+1 && gameP1>=6)
                     {
                         setP1++;
@@ -179,41 +193,67 @@ namespace Tennis
         }
 
         static void ScoreBoard(byte pointP1,byte pointP2)
-        {               
-            Console.Clear();
-            switch (pointP1)
+        {
+            char advantageP1 = ' ';
+            char advantageP2 = ' ';
+            byte SbPointP1 = 0;
+            byte SbPointP2 = 0;
+              Console.Clear();
+              switch (pointP1)
+              {
+                  case 1:
+                      SbPointP1 = 15;
+                      break;
+                  case 2:
+                      SbPointP1 = 30;
+                      break;
+                  case 3:
+                      SbPointP1 = 40;
+                      break;
+                  case 4:
+                      SbPointP1 = 0;
+                      break;
+              }          
+              switch (pointP2)
+              {
+                  case 1:
+                    SbPointP2 = 15;
+                    break;
+                  case 2:
+                    SbPointP2 = 30;
+                    break;
+                  case 3:
+                    SbPointP2 = 40;
+                    break;
+                  case 4:
+                    SbPointP2 = 0;
+                    break;
+              }
+            if (pointP1 >= 3 && pointP2 >= 3)
             {
-                case 1:
-                    pointP1 = 15;  
-                    break;
-                case 2:
-                    pointP1 = 30;
-                    break;
-                case 3:
-                    pointP1 = 40;
-                    break;
-                case 4:
-                    pointP1 = 0;
-                    break;
-            }
-            switch (pointP2)
-            {
-                case 1:
-                    pointP2 = 15;
-                    break;
-                case 2:
-                    pointP2 = 30;
-                    break;
-                case 3:
-                    pointP2 = 40;
-                    break;
-                case 4:
-                    pointP2 = 0;
-                    break;
+                Console.Clear();
+                if (pointP1 == pointP2)
+                {
+                    SbPointP1 = 40;
+                    SbPointP2 = 40;
+                }
+                if (pointP1 == pointP2 + 1)
+                {
+                    SbPointP1 = 40;
+                    SbPointP2 = 40;
+                    advantageP1 = 'A';
+                }
+                if (pointP2 == pointP1 + 1)
+                {
+                    SbPointP2 = 40;
+                    SbPointP1 = 40;
+                    advantageP2 = 'A';
+                }
+
             }
             Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------\n");
-            Console.WriteLine(FiggleFonts.Basic.Render($"| {player[0].Substring(0,2).ToUpper()} | {GameP1History[0]} | {GameP1History[1]} | {GameP1History[2]} | {GameP1History[3]} | {GameP1History[4]} | {pointP1}"));
-            Console.WriteLine(FiggleFonts.Basic.Render($"| {player[1].Substring(0,2).ToUpper()} | {GameP2History[0]} | {GameP2History[1]} | {GameP2History[2]} | {GameP2History[3]} | {GameP2History[4]} | {pointP2}"));
+            Console.WriteLine(FiggleFonts.Basic.Render($"| {player[0].Substring(0,2).ToUpper()} | {GameP1History[0]} | {GameP1History[1]} | {GameP1History[2]} | {GameP1History[3]} | {GameP1History[4]} | {SbPointP1} | {advantageP1}"));
+            Console.WriteLine(FiggleFonts.Basic.Render($"| {player[1].Substring(0,2).ToUpper()} | {GameP2History[0]} | {GameP2History[1]} | {GameP2History[2]} | {GameP2History[3]} | {GameP2History[4]} | {SbPointP2} | {advantageP2}"));
             Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------");
         }
     }
