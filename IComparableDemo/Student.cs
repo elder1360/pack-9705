e49574950570cb4a51880ccc7fb24432fc665f5b
Elder1360 <mohamm60@outlook.com>
2018-10-10 00:24:16 +0330

    IComparableDemo

---
+++ IComparableDemo/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IComparableDemo
+{
+    class Student:IComparable
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public double Average { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Student otherStudent = obj as Student;
+            if (obj!=null)
+            {
+                return this.Name.CompareTo(otherStudent.Name);
+            }
+            return -1;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Family} [{Average}]";
+        }
+    }
+}
