bbde3543bbff3cfaa40a2ea15dde294d98c4cfb8
Elder1360 <mohamm60@outlook.com>
2018-10-28 23:38:38 +0330

    My personal website

---
+++ My WEB SITE/js/main.js
@@ -0,0 +1,92 @@
+jQuery(document).ready(function( $ ) {
+
+  // Smooth scroll for the menu and links with .scrollto classes
+  $('.smothscroll').on('click', function(e) {
+    e.preventDefault();
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - 62
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  $('.carousel').carousel({
+    interval: 3500
+  });
+
+  // JavaScript Chart
+  var doughnutData = [{
+      value: 40,
+      color: "#1abc9c"
+    },
+    {
+      value: 60,
+      color: "#ecf0f1"
+    }
+  ];
+  var myDoughnut = new Chart(document.getElementById("javascript").getContext("2d")).Doughnut(doughnutData);
+
+  // Bootstrap Chart
+  var doughnutData = [{
+    value: 40,
+    color: "#1abc9c"
+  },
+  {
+    value: 60,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("bootstrap").getContext("2d")).Doughnut(doughnutData);
+
+  // C# Chart
+  var doughnutData = [{
+    value: 65,
+    color: "#1abc9c"
+  },
+  {
+    value: 35,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("c#").getContext("2d")).Doughnut(doughnutData);
+
+  // HTML Chart
+  var doughnutData = [{
+    value: 50,
+    color: "#1abc9c"
+  },
+  {
+    value: 50,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("html").getContext("2d")).Doughnut(doughnutData);
+
+  // sqlserver Chart
+  var doughnutData = [{
+    value: 60,
+    color: "#1abc9c"
+  },
+  {
+    value: 40,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("sqlserver").getContext("2d")).Doughnut(doughnutData);
+
+  // asp.net Chart
+  var doughnutData = [{
+    value: 5,
+    color: "#1abc9c"
+  },
+  {
+    value: 95,
+    color: "#ecf0f1"
+  }
+  ];
+  var myDoughnut = new Chart(document.getElementById("asp.net").getContext("2d")).Doughnut(doughnutData);
+});
